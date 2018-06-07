f7bff3b508eb62c915c6a2e2252ad77c3905a1de
linzhihuang <694982665@qq.com>
2018-06-07 14:53:58 +0800

    定时同步任务

--- fino-cdm-ntp/Dockerfile
+++ fino-cdm-ntp/Dockerfile
@@ -2,6 +2,8 @@ FROM docker.finogeeks.club/linzhihuang/fino-cdm-envbase:v1.0
 MAINTAINER linzhihuang
 RUN mkdir -p /opt/cdm/
 COPY initfiles/init.sh /opt/cdm/
+COPY initfiles/sync.cnf /opt/cdm/
 RUN chmod a+x /opt/cdm/init.sh
+RUN apt-get install -y cron
 expose 123
 CMD ["sh","-c","/opt/cdm/init.sh* && bash"]
--- fino-cdm-ntp/initfiles/init.sh
+++ fino-cdm-ntp/initfiles/init.sh
@@ -4,7 +4,6 @@
 if [ "$NODE_TYPE" == "server" ]; then
 rm /etc/ntp.conf
 cat >> /etc/ntp.conf <<EOF
-# 中国这边最活跃的时间服务器 : http://www.pool.ntp.org/zone/cn
 server 0.ubuntu.pool.ntp.org 
 server 1.ubuntu.pool.ntp.org 
 server 2.ubuntu.pool.ntp.org 
@@ -15,7 +14,6 @@ restrict 0.asia.pool.ntp.org nomodify notrap noquery
 restrict 3.asia.pool.ntp.org nomodify notrap noquery
 # Undisciplined Local Clock. This is a fake driver intended for backup
 # and when no outside source of synchronized time is available.
-# 外部时间服务器不可用时，以本地时间作为时间服务
 server  127.127.1.0     # local clock
 fudge   127.127.1.0 stratum 10
 EOF
@@ -25,3 +23,5 @@ else
     echo "restrict ${NTP_SERVER_HOSTNAME} " >> /etc/ntp.conf
 fi
 service ntp restart
+service cron restart
+crontab /opt/cdm/sync.cnf
